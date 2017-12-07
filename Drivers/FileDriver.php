956dc81c49ba2e44761156576d7eed31922e96d0
Wolfram Eberius <w.eberius@dynamic-designer.de>
2017-12-07 17:43:30 +0100

    Fixed exception message.

--- Drivers/FileDriver.php
+++ Drivers/FileDriver.php
@@ -16,7 +16,7 @@ class FileDriver extends AbstractDriver
         parent::__construct($options);
 
         if ( ! isset($options['file_path'])) {
-            throw new \InvalidArgumentException('$options[\'file_path\'] cannot be defined if Driver File configuration is used');
+            throw new \InvalidArgumentException('$options[\'file_path\'] must be defined if Driver File configuration is used.');
         }
         if (null !== $options) {
             $this->filePath = $options['file_path'];
